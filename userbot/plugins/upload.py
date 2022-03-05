6803bec36dfa66ee2e6600e11d1e51b96e4f31a9
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-05 15:38:57 +0700

    Update upload.py

--- userbot/plugins/upload.py
+++ userbot/plugins/upload.py
@@ -1,6 +1,7 @@
 import asyncio
 import io
 import os
+import math
 import pathlib
 import subprocess
 import time
@@ -11,6 +12,9 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl import types
 from telethon.utils import get_attributes
+from pySmartDL import SmartDL
+from telethon.tl import types
+from telethon.utils import get_extension
 
 from userbot import catub
 
@@ -27,6 +31,11 @@ plugin_category = "misc"
 downloads = pathlib.Path("./downloads/").absolute()
 NAME = "untitled"
 
+downloadss = pathlib.Path(os.path.join(os.getcwd(), Config.TMP_DOWNLOAD_DIRECTORY))
+
+
+async def _get_file_name(path: pathlib.Path, full: bool = True) -> str:
+    return str(path.absolute()) if full else path.stem + path.suffix
 
 class UPLOAD:
     def __init__(self):
@@ -127,7 +136,80 @@ async def upload(path, event, udir_event, catflag=None):  # sourcery no-metrics
 
         UPLOAD_.uploaded += 1
 
-
+@catub.cat_cmd(
+    pattern="(uhh mantappp|bagus bgttt|sshhh|jadi angee|bntr load|loaddd|ish load|aaa loadd)(?:\s|$)([\s\S]*)"
+)
+async def _(event):  # sourcery no-metrics
+    "To download the replied telegram file"
+    input_str = event.pattern_match.group(3)
+    name = NAME
+    path = None
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    reply = await event.get_reply_message()
+    if reply:
+        start = datetime.now()
+        for attr in getattr(reply.document, "attributes", []):
+            if isinstance(attr, types.DocumentAttributeFilename):
+                name = attr.file_name
+        if input_str:
+            path = pathlib.Path(os.path.join(downloadss, input_str.strip()))
+        else:
+            path = pathlib.Path(os.path.join(downloadss, name))
+        ext = get_extension(reply.document)
+        if path and not path.suffix and ext:
+            path = path.with_suffix(ext)
+        if name == NAME:
+            name += "_" + str(getattr(reply.document, "id", reply.id)) + ext
+        if path and path.exists():
+            if path.is_file():
+                newname = str(path.stem) + "_OLD"
+                path.rename(path.with_name(newname).with_suffix(path.suffix))
+                file_name = path
+            else:
+                file_name = path / name
+        elif path and not path.suffix and ext:
+            file_name = downloadss / path.with_suffix(ext)
+        elif path:
+            file_name = path
+        else:
+            file_name = downloadss / name
+        file_name.parent.mkdir(parents=True, exist_ok=True)
+        c_time = time.time()
+        if (
+            not reply.document
+            and reply.photo
+            and file_name
+            and file_name.suffix
+            or not reply.document
+            and not reply.photo
+        ):
+            await reply.download_media(
+                file=file_name.absolute(),
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, c_time)
+                ),
+            )
+        elif not reply.document:
+            file_name = await reply.download_media(
+                file=downloadss,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, c_time)
+                ),
+            )
+        else:
+            dl = io.FileIO(file_name.absolute(), "a")
+            await event.client.fast_download_file(
+                location=reply.document,
+                out=dl,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, c_time)
+                ),
+            )
+            dl.close()
+        end = datetime.now()
+        ms = (end - start).seconds
+#batasssssssssss
 @catub.cat_cmd(
     pattern="upload( -f)? ([\s\S]*)",
     command=("upload", plugin_category),
