bc3f6b3ca6fff0f7f315edc210890552e49337db
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-15 02:10:45 +0700

    Update nganu.py

--- userbot/plugins/nganu.py
+++ userbot/plugins/nganu.py
@@ -30,9 +30,9 @@ async def _get_file_name(path: pathlib.Path, full: bool = True) -> str:
 )
 async def _(event):  # sourcery no-metrics
     "To download the replied telegram file"
-    mone = await event.get_reply_message()
+    #mone = await event.get_reply_message()
     # sal = print("download file")
-     mone = await edit_or_reply(event, "loaddddd :(")
+    mone = await edit_or_reply(event, "loaddddd :(")
     input_str = event.pattern_match.group(3)
     name = NAME
     path = None
