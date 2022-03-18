e20ad783f8195e0538ea6820897d1aed31f1cc97
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 01:43:28 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -4,7 +4,7 @@ from pytgcalls import idle
 
 import userbot
 from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
-form . import call_py
+from . import call_py
 from .Config import Config
 from .core.logger import logging
 from .core.session import catub
