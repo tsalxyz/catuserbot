11ab655165c68a2653b8aafe5f9e12b9b228bcc4
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 01:57:17 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -43,10 +43,10 @@ async def startup_process():
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
-    await call_py.start()
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
+    await call_py.start()
     await idle()
     return
 
