008565835b76edf1570115fe8985d16c5b54bd53
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-18 01:06:08 -0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,7 +1,7 @@
 import signal
 import sys
 import time
-
+import os
 import heroku3
 from pytgcalls import PyTgCalls
 
@@ -73,6 +73,14 @@ try:
 except Exception:
     HEROKU_APP = None
 
+# Picture For VCPLUGIN
+PLAY_PIC = (
+    os.environ.get("PLAY_PIC") or "https://telegra.ph/file/6213d2673486beca02967.png"
+)
+
+QUEUE_PIC = (
+    os.environ.get("QUEUE_PIC") or "https://telegra.ph/file/d6f92c979ad96b2031cba.png"
+)
 # Global Configiables
 COUNT_MSG = 0
 USERS = {}
