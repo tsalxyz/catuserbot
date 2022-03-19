4f2eaf36aaba1bdb532c18cb07bd827494214a37
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2022-03-19 01:24:46 +0000

    pylint: auto fixes

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -4,8 +4,8 @@ from pytgcalls import idle
 
 import userbot
 from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
-
 from userbot.utils import call_py
+
 from .Config import Config
 from .core.logger import logging
 from .core.session import catub
