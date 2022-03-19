47959c6775137688a24f1d056cb0d070ae19ff8a
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 14:20:30 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,6 +1,6 @@
 import sys
 
-from pytgcalls import idle
+#from pytgcalls import idle
 
 import userbot
 from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
@@ -49,7 +49,7 @@ async def startup_process():
 
 
 catub.loop.run_until_complete(startup_process())
-idle()
+#idle()
 if len(sys.argv) not in (1, 3, 4):
     catub.disconnect()
 else:
