e174959e8047623b7662eb9cded9d6ba0006191b
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 08:41:44 +0700

    Update core.py

--- userbot/utils/core.py
+++ userbot/utils/core.py
@@ -1,8 +1,15 @@
+import os
+import re
+import sys
+
+from requests import get
 from pytgcalls import PyTgCalls
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
+
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
 if STRING_SESSION:
     session = StringSession(str(STRING_SESSION))
     bot = TelegramClient(
