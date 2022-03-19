4a35df68f567af971ba9de8ccb6661d3ea0895eb
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 08:23:11 +0700

    Create core.py

---
+++ userbot/utils/core.py
@@ -0,0 +1,16 @@
+from telethon.sync import TelegramClient
+from pytgcalls import PyTgCalls
+
+
+
+if STRING_SESSION: 
+  session = StringSession(str(STRING_SESSION))
+  bot = TelegramClient( 
+    session=session, 
+    api_id=Config.APP_ID, 
+    api_hash=API_HASH, 
+    connection=ConnectionTcpAbridged, 
+    connection_retries=None, 
+    auto_reconnect=True, 
+  ) 
+  call_py = PyTgCalls(bot, overload_quiet_mode=True)
