1dcd5e9383c4dbac1012de06fec2def6e952af4e
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 12:44:35 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,7 +8,7 @@ from userbot import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 
 from .Config import Config
 from .core.logger import logging
-from .core.session import call_py, catub
+from .core.session import catub
 from .utils import (
     add_bot_to_logger_group,
     load_plugins,
@@ -34,7 +34,7 @@ except Exception as e:
 
 
 async def startup_process():
-    await call_py.start()
+    await catub.start()
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
@@ -51,10 +51,8 @@ async def startup_process():
     return
 
 
-# catub.loop.run_until_complete(startup_process())
-if __name__ == "session":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(startup_process())
+catub.loop.run_until_complete(startup_process())
+
 if len(sys.argv) not in (1, 3, 4):
     catub.disconnect()
 else:
