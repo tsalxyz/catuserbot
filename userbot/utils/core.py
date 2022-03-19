49104809bc200ef9ac2c30972327f365cbf9705d
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-19 08:56:06 +0700

    Update core.py

--- userbot/utils/core.py
+++ userbot/utils/core.py
@@ -5,15 +5,16 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-from .Config import Config
 
+APP_ID = int(os.environ.get("APP_ID", 6)) 
+API_HASH = os.environ.get("API_HASH") or None
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 if STRING_SESSION:
     session = StringSession(str(STRING_SESSION))
     bot = TelegramClient(
         session=session,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
+        api_id=APP_ID,
+        api_hash=API_HASH,
         connection=ConnectionTcpAbridged,
         connection_retries=None,
         auto_reconnect=True,
