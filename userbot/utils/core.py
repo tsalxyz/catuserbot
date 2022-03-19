5adaa587a183d9d0947f43b4aa5eb933a59e360d
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2022-03-19 01:23:54 +0000

    pylint: auto fixes

--- userbot/utils/core.py
+++ userbot/utils/core.py
@@ -1,16 +1,14 @@
-from telethon.sync import TelegramClient
 from pytgcalls import PyTgCalls
+from telethon.sync import TelegramClient
 
-
-
-if STRING_SESSION: 
-  session = StringSession(str(STRING_SESSION))
-  bot = TelegramClient( 
-    session=session, 
-    api_id=Config.APP_ID, 
-    api_hash=API_HASH, 
-    connection=ConnectionTcpAbridged, 
-    connection_retries=None, 
-    auto_reconnect=True, 
-  ) 
-  call_py = PyTgCalls(bot, overload_quiet_mode=True)
+if STRING_SESSION:
+    session = StringSession(str(STRING_SESSION))
+    bot = TelegramClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=API_HASH,
+        connection=ConnectionTcpAbridged,
+        connection_retries=None,
+        auto_reconnect=True,
+    )
+    call_py = PyTgCalls(bot, overload_quiet_mode=True)
