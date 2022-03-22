6db3eee9680df81590f2d69c73568d86b2b411e5
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-22 15:57:03 +0700

    Update stickerfun.py

--- userbot/plugins/stickerfun.py
+++ userbot/plugins/stickerfun.py
@@ -113,12 +113,15 @@ async def penguin(event):
 
 
 @catub.cat_cmd(
-    pattern="ist ?(.*)",
+    pattern="1ist ?(.*)",
     command=("ist", plugin_category),
     info={
         "header": "Inline Write-On Sticker",
         "usage": [
             "{tr}ist <your text>",
+            "{tr}2ist <your text>",
+            "{tr}3ist <your text>",
+            "{tr}4ist <your text>",
         ],
     },
 )
@@ -126,14 +129,30 @@ async def isong(event):
     if event.fwd_from:
         return
     bot = "QuotAfBot"
-    text = event.pattern_match.group(1)
+    cmd = event.pattern_match.group(1).lower()
+    text = event.pattern_match.group(2)
     reply_to_id = await reply_id(event)
     if not text:
         await edit_delete(event, "`Give me a text`")
     else:
         await event.delete()
         run = await event.client.inline_query(bot, text)
+        result = await run[0].click("me")
+        await event.client.send_message(event.chat_id, result, reply_to=reply_to_id)
+        await result.delete()
+    if cmd == "2":
+        run = await event.client.inline_query(bot, text)
         result = await run[1].click("me")
+        await event.client.send_message(event.chat_id, result, reply_to=reply_to_id)
+        await result.delete()
+    if cmd == "3":
+        run = await event.client.inline_query(bot, text)
+        result = await run[2].click("me")
+        await event.client.send_message(event.chat_id, result, reply_to=reply_to_id)
+        await result.delete()
+    if cmd == "4":
+        run = await event.client.inline_query(bot, text)
+        result = await run[3].click("me")
         await event.client.send_message(event.chat_id, result, reply_to=reply_to_id)
         await result.delete()
 
