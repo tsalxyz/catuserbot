e50482204025ca8b4de010abded5851437492695
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-25 21:19:18 +0700

    Update broadcast.py

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -42,8 +42,8 @@ async def gcast(event):
         if x.is_group:
             chat = x.id
             try:
-                done += 1
                 await event.client.send_message(chat, msg)
+                done += 1
                 await asyncio.sleep(0.1)
             except FloodWaitError:
                 await sleep(0.5)
