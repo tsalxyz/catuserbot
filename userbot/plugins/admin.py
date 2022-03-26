c69365c27f67929e229c434c372d235b3fd1e571
tsalxyz <87889510+tsalxyz@users.noreply.github.com>
2022-03-26 23:17:42 +0700

    Update admin.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -34,7 +34,7 @@ from . import BOTLOG, BOTLOG_CHATID
 PP_TOO_SMOL = "`The image is too small`"
 PP_ERROR = "`Failure while processing the image`"
 NO_ADMIN = "`I am not an admin nub nibba!`"
-NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play despacito`"
+NO_PERM = "`I don't have sufficient permissions!`"
 CHAT_PP_CHANGED = "`Chat Picture Changed`"
 INVALID_MEDIA = "`Invalid Extension`"
 
